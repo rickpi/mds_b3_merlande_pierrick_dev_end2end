53c835d3c7e5c6449e18a0792b481ae5fe8fbef2
Pierrick MERLANDE <merlandepierrick@gmail.com>
2021-01-22 09:52:34 +0100

    conf nightwatch with browserstack

---
+++ nightwatch.conf.js
@@ -0,0 +1,19 @@
+module.exports = {
+  src_folders: ['tests'],
+  selenium: {
+    start_process: true,
+    server_path: require('selenium-server').path,
+    host: 'hub-cloud.browserstack.com',
+    port: 443
+  },
+
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        'browserstack.user': 'pierrickmerlande1',
+        'browserstack.key': 'qTsAa9hsFu3mYy3nzu53',
+        'browser': 'chrome',
+      }
+    }
+  }
+};
---
+++ tests/test.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'Google\'s Search Functionality' : function (browser) {
+    browser
+      .url('https://www.google.com/ncr')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'BrowserStack')
+      .click('input[name=btnK]')
+      .pause(1000)
+      .assert.title('BrowserStack - Google Search')
+      .end();
+  }
+};
