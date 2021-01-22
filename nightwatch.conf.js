6f012aad49177d5e82addcc920afa3fdd33ae0f6
Pierrick MERLANDE <merlandepierrick@gmail.com>
2021-01-22 14:49:17 +0100

    conf: change screen resolution

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -4,15 +4,21 @@ module.exports = {
     start_process: true,
     server_path: require('selenium-server').path,
     host: 'hub-cloud.browserstack.com',
-    port: 80
+    port: 80,
   },
 
   test_settings: {
     default: {
       desiredCapabilities: {
+        'os': 'WINDOWS',
+        'os_version': '10',
+        'resolution': '1366x768',
         'browserstack.user': 'pierrickmerlande1',
         'browserstack.key': 'qTsAa9hsFu3mYy3nzu53',
         'browser': 'chrome',
+        "chromeOptions": {
+          'args': ['start-maximized']
+        }
       }
     }
   }
