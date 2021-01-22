b2e1cb975e52a36a2de9bc2f7d2a24043041c79b
Pierrick MERLANDE <merlandepierrick@gmail.com>
2021-01-22 15:08:25 +0100

    test: check login block in two sections

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,7 @@ describe('Uber', function() {
 
   test('Open Uber Website', function (browser) {
     browser
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('body')
       .assert.elementPresent('title')
       .assert.elementPresent('head')
       .end();
@@ -12,10 +12,23 @@ describe('Uber', function() {
 
   test('Check block login in the header wrapper', function (browser) {
     browser
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('body')
       .useXpath()
-      .assert.elementPresent('//*[@id="main"]/nav/div/ul[4]/li[4]/button', 'Sign in button exist')
-      .assert.elementPresent('//*[@id="main"]/nav/div/ul[4]/li[5]/button', 'Sign up button exist')
+      .assert.visible('//*[@id="main"]/nav/div/ul[4]/li[4]/button', 'Sign in button is visible')
+      .assert.visible('//*[@id="main"]/nav/div/ul[4]/li[5]/button', 'Sign up button is visible')
+      .useCss()
+      .end();
+  });
+
+  test('Check login block in two sections', function (browser) {
+    browser
+      .waitForElementVisible('body')
+      .useXpath()
+      .assert.visible('//*[@id="main"]/nav/div/ul[4]/li[4]/button', 'Sign in button is visible')
+      .click('//*[@id="main"]/nav/div/ul[4]/li[4]/button', () => 'Click on sign in button')
+      .assert.visible('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/div[4]/section/div/div/div/div/div[1]/a', 'Driver sign in button is visible')
+      .assert.visible('//*[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/div[4]/section/div/div/div/div/div[3]/a', 'Passenger sign in button is visible')
+      .useCss()
       .end();
   });
 });
