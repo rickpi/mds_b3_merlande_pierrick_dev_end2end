5c2f61826a8aa16ac96adbcecc091ecfaf1f3cc3
Pierrick MERLANDE <merlandepierrick@gmail.com>
2021-01-22 14:49:38 +0100

    test: check block login in the header wrapper

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,21 @@
-module.exports = {
-  'Open Uber Website' : function (browser) {
+describe('Uber', function() {
+
+  beforeEach(browser => browser.url('https://www.uber.com/fr/fr/'));
+
+  test('Open Uber Website', function (browser) {
     browser
-      .url('https://www.uber.com/fr/fr/')
       .waitForElementVisible('body', 1000)
       .assert.elementPresent('title')
       .assert.elementPresent('head')
       .end();
-  }
-};
+  });
+
+  test('Check block login in the header wrapper', function (browser) {
+    browser
+      .waitForElementVisible('body', 1000)
+      .useXpath()
+      .assert.elementPresent('//*[@id="main"]/nav/div/ul[4]/li[4]/button', 'Sign in button exist')
+      .assert.elementPresent('//*[@id="main"]/nav/div/ul[4]/li[5]/button', 'Sign up button exist')
+      .end();
+  });
+});
